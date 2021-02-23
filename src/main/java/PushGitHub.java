474900870adff44ee2a900c50f5b725290f11884
yyx-bigdata <1353900744@qq.com>
2021-02-23 19:58:38 +0800

    idea首次上传

---
+++ src/main/java/PushGitHub.java
@@ -0,0 +1,9 @@
+public class PushGitHub {
+    public static void main(String[] args) {
+        System.out.println("你好1");
+        System.out.println("你好2");
+        System.out.println("你好3");
+        System.out.println("你好4");
+        System.out.println("你好5");
+    }
+}
